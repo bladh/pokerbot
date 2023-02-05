85ef4b604585fe1110a64e30000cfc23fff40bcd
Alexander Bladh <bladh.alexander@gmail.com>
2023-02-05 21:06:42 +0800

    Remove mentions of nickname from poker code (#24)
    Code in the poker package will only refer to the Player class
    when referring to players and not interchangeably refer to
    a nickname. This should be the final separation from irc logic
    in the poker code.

--- src/main/java/me/ars/pokerbot/poker/StateCallback.java
+++ src/main/java/me/ars/pokerbot/poker/StateCallback.java
@@ -7,25 +7,25 @@ public interface StateCallback {
     /**
      * A player has called a bet. If the player didn't have enough money, then [money] will be lower than [owed].
      *
-     * @param nick  Nickname of caller
-     * @param money Amount called
+     * @param player  Player who called
+     * @param money   Amount called
      */
-    void playerCalled(String nick, int money);
+    void playerCalled(Player player, int money);
 
     /**
      * A player has raised a bet.
      *
-     * @param name     Player who raised
+     * @param player   Player who raised
      * @param newRaise Amount that was raised
      */
-    void playerRaised(String name, int newRaise);
+    void playerRaised(Player player, int newRaise);
 
     /**
      * A player has checked.
      *
-     * @param name Checking player
+     * @param player Checking player
      */
-    void playerChecked(String name);
+    void playerChecked(Player player);
 
     /**
      * Announce a message to all players.
@@ -41,94 +41,94 @@ public interface StateCallback {
      * @param pot           Current pot on the table
      * @param currentPlayer Current players turn
      */
-    void updateTable(List<Card> table, int pot, String currentPlayer);
+    void updateTable(List<Card> table, int pot, Player currentPlayer);
 
     /**
      * Notify that a player must call a raise.
      *
-     * @param name       Player who needs to call
+     * @param player     Player who needs to call
      * @param amountOwed Amount of money needed to call
      */
-    void mustCallRaise(String name, int amountOwed);
+    void mustCallRaise(Player player, int amountOwed);
 
     /**
      * Announce that a player could not raise the specified bet.
      *
-     * @param name   Player who tried to bet
+     * @param player Player who tried to bet
      * @param money  Amount of money they actually had
      */
-    void playerCannotRaise(String name, int money);
+    void playerCannotRaise(Player player, int money);
 
     /**
      * A player has gone all in.
      *
-     * @param name Name of allin player
+     * @param player Player who went all in.
      */
-    void playerAllin(String name);
+    void playerAllin(Player player);
 
     /**
      * A player has folded.
      *
-     * @param name Name of folding player
+     * @param player Folding player
      */
-    void playerFolded(String name);
+    void playerFolded(Player player);
 
     /**
      * A player cashed out and left the table.
      *
-     * @param name  Name of cashing out player
+     * @param player Cashing out player
      * @param money Amount of money they walked away with
      */
-    void playerCashedOut(String name, int money);
+    void playerCashedOut(Player player, int money);
 
     /**
      * Show the player the two cards they were dealt.
      *
-     * @param name  Name of player
+     * @param player Player receiving their cards
      * @param card1 First card
      * @param card2 Second card
      * @param spycard If playing with spycards
      */
-    void showPlayerCards(String name, Card card1, Card card2, Card spycard);
+    void showPlayerCards(Player player, Card card1, Card card2, Card spycard);
 
     /**
      * Display the currently playing players and the money they have
      *
-     * @param players Map of player names to how much money they have
+     * @param players Map of players to how much money they have
      */
-    void showPlayers(Map<String, Integer> players);
+    void showPlayers(Map<Player, Integer> players);
 
     /**
      * Reveals the hands of the supplied players.
      *
-     * @param reveal Names mapped to their own cards
+     * @param reveal Players mapped to their own cards
      */
-    void revealPlayers(Map<String, List<Card>> reveal);
+    void revealPlayers(Map<Player, List<Card>> reveal);
 
     /**
      * Declare that a player has won the pot
      *
-     * @param name        Name of winner
+     * @param winner      Winning player
      * @param winningHand Their winning hand
      * @param pot         Money they've won from the pot
      */
-    void declareWinner(String name, Hand winningHand, int pot);
+    void declareWinner(Player winner, Hand winningHand, int pot);
 
     /**
      * Declare that there are multiple winners splitting the pot
      *
-     * @param winners  Name of all winners
+     * @param winners  List of winning players
      * @param handType The hand type they had in common
      * @param pot      The pot they are splitting
      */
-    void declareSplitPot(List<String> winners, Hand.HandType handType, int pot);
+    void declareSplitPot(List<Player> winners, Hand.HandType handType, int pot);
 
     /**
-     * Declares whos turn it is.
+     * Declares who's turn it is.
      *
      * @param player Current player
      */
-    void declarePlayerTurn(String player);
+    void declarePlayerTurn(Player player);
 
     /**
      * Declare that ante is being collected
@@ -145,5 +145,12 @@ public interface StateCallback {
      * @param smallBlindPlayer The player that pays the small blind
      * @param smallBlind       How big the small blind is
      */
-    void collectBlinds(String bigBlindPlayer, int bigBlind, String smallBlindPlayer, int smallBlind);
+    void collectBlinds(Player bigBlindPlayer, int bigBlind, Player smallBlindPlayer, int smallBlind);
+
+    /**
+     * Declare that the game has ended.
+     *
+     * @param oldPlayers Players that have left the ended game
+     */
+    void gameEnded(List<Player> oldPlayers);
 }
